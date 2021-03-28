a3917ed680c4550b75636b4779bd152de6680ce5
helloteacher-alt <panyisa20012544@gmail.com>
2021-03-28 20:34:44 +0700

    final

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Exam1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            Console.Write("Input a:");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Input b:");
+            double b = double.Parse(Console.ReadLine());
+            x = a;
+            y = b;
+            while(x != y)
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else y = y + b;
+            }
+            Console.WriteLine(x);
+                
+
+
+           
+        }
+    }
+}
